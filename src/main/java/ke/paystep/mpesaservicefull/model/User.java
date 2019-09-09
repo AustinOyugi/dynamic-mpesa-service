819a74fce33c8a73a1ab143791b25c54b79ffe53
AustinMartins <austinoyugi@gmail.com>
2019-09-09 13:58:19 +0300

    Updates

--- src/main/java/ke/paystep/mpesaservicefull/model/User.java
+++ src/main/java/ke/paystep/mpesaservicefull/model/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
  * Created by Austin Oyugi on 17/8/2019.
  */
 @Entity
-@Table(name = "User", uniqueConstraints = {
+@Table(name = "user", uniqueConstraints = {
         @UniqueConstraint(columnNames = {
                 "userName"
         }),
