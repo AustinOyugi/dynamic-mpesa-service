934fab93d7dab3447349ed0cd04f7e86760a9661
Austin Oyugi <austinoyugi@gmail.com>
2020-05-06 08:58:03 +0300

    Updates

--- src/main/java/ke/paystep/mpesaservicefull/controller/C2BController.java
+++ src/main/java/ke/paystep/mpesaservicefull/controller/C2BController.java
@@ -38,12 +38,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/payment/mpesa/c2b")
-public class C2BController
-{
+public class C2BController {
     private static final Logger LOGGER = LoggerFactory.getLogger(C2BController.class);
 
     @Value("${api-settings.url}")
-    private  String api_url;
+    private String api_url;
 
     @Value("${mpesa.business_short_code}")
     private String business_short_code;
@@ -69,21 +68,20 @@ public class C2BController
             @io.swagger.annotations.ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")
     })
-    public ResponseEntity<?> registerUrl()
-    {
+    public ResponseEntity<?> registerUrl() {
         JSONObject jsonObject;
         String responseType = "Completed";
-        String confirmationUrl = api_url+"/v1/payment/m/c2b/url/confirmation";
-        String validationUrl = api_url+"/v1/payment/m/c2b/url/validation";
+        String confirmationUrl = api_url + "/v1/payment/m/c2b/url/confirmation";
+        String validationUrl = api_url + "/v1/payment/m/c2b/url/validation";
 
         try {
             jsonObject = c2BTransaction.url_registration(business_short_code, responseType, confirmationUrl, validationUrl);
         } catch (IOException e) {
             LOGGER.error("URL registration Failed {}", e.getLocalizedMessage());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"Internal Server Error"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "Internal Server Error"));
         }
 
-        return new ResponseEntity<>( jsonObject.toMap(), HttpStatus.OK);
+        return new ResponseEntity<>(jsonObject.toMap(), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Use this API to initiate online payment on behalf of a customer.")
@@ -96,41 +94,37 @@ public class C2BController
             @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request")
     })
     @PostMapping("/stkpush/initiate")
-    public ResponseEntity<?> stkPushInitiate(@CurrentUser UserPrincipal userPrincipal ,
-                                             @RequestBody StkPushRequest stkPushRequest)
-    {
-        if (!UrlValidator.urlValidator(stkPushRequest.getConfirmationUrl()))
-        {
-            return ResponseEntity.status(400).body(new ApiResponse(false,"Enter a Valid URL"));
+    public ResponseEntity<?> stkPushInitiate(@CurrentUser UserPrincipal userPrincipal,
+                                             @RequestBody StkPushRequest stkPushRequest) {
+        if (!UrlValidator.urlValidator(stkPushRequest.getConfirmationUrl())) {
+            return ResponseEntity.status(400).body(new ApiResponse(false, "Enter a Valid URL"));
         }
 
         JSONObject jsonObject;
 
         try {
             jsonObject = c2BTransaction.payment(stkPushRequest);
-            
+
         } catch (IOException e) {
             LOGGER.error("Transaction Failed {}", e.getLocalizedMessage());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"Internal Server Error"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "Internal Server Error"));
         }
 
         assert jsonObject != null;
         int responseCode;
-        
-        try{
+
+        try {
             responseCode = Integer.parseInt(jsonObject.getString("ResponseCode"));
-            
-        }catch(Exception e)
-        {
+
+        } catch (Exception e) {
             LOGGER.info(jsonObject.toString());
             LOGGER.error(e.getLocalizedMessage());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"An Error Occurred, Try Again"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "An Error Occurred, Try Again"));
         }
-        
-        if (responseCode != 0)
-        {
+
+        if (responseCode != 0) {
             LOGGER.error(jsonObject.toString());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"An Error Occurred, Try Again"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "An Error Occurred, Try Again"));
         }
 
         StkPushModel stkPushModel = new StkPushModel();
@@ -157,8 +151,7 @@ public class C2BController
             @io.swagger.annotations.ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
             @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")
     })
-    public void stkPushResult(@RequestBody String jsonString)
-    {
+    public void stkPushResult(@RequestBody String jsonString) {
         JSONObject jsonObject = new JSONObject(jsonString);
         JSONObject jsonChildObject = (JSONObject) jsonObject.get("Body");
         JSONObject jsonFirstChild = (JSONObject) jsonObject.get("stkCallback");
@@ -166,17 +159,16 @@ public class C2BController
         int resultCode = Integer.parseInt(jsonFirstChild.getString("ResultCode"));
         String checkoutRequestID = jsonChildObject.getString("CheckoutRequestID");
 
-        StkPushModel  stkPushModel = stkPushRepository.getByCheckoutRequestID(checkoutRequestID).orElse(null);
+        StkPushModel stkPushModel = stkPushRepository.getByCheckoutRequestID(checkoutRequestID).orElse(null);
         assert stkPushModel != null;
         String url = stkPushModel.getConfirmationUrl();
 
-        if (resultCode == 0)
-        {
+        if (resultCode == 0) {
             stkPushModel.setTransactionComplete((short) 1);
             stkPushRepository.save(stkPushModel);
         }
 
-        restTemplate.postForEntity(url,jsonString,String.class);
+        restTemplate.postForEntity(url, jsonString, String.class);
     }
 
     @ApiOperation("Use this API to check the status of a Lipa Na M-Pesa Online Payment.")
@@ -188,11 +180,9 @@ public class C2BController
             @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")
     })
     @PostMapping("/stkpush/query")
-    public ResponseEntity<?> queryPaymentStatus(@Valid @RequestBody StkPushQueryRequest stkPushQueryRequest)
-    {
-        if (stkPushRepository.getByCheckoutRequestID(stkPushQueryRequest.getCheckoutRequestID()).isPresent())
-        {
-            return  ResponseEntity.status(404).body(new ResourceNotFoundException("CheckoutRequest", "Id", stkPushQueryRequest.getCheckoutRequestID()));
+    public ResponseEntity<?> queryPaymentStatus(@Valid @RequestBody StkPushQueryRequest stkPushQueryRequest) {
+        if (stkPushRepository.getByCheckoutRequestID(stkPushQueryRequest.getCheckoutRequestID()).isPresent()) {
+            return ResponseEntity.status(404).body(new ResourceNotFoundException("CheckoutRequest", "Id", stkPushQueryRequest.getCheckoutRequestID()));
         }
 
         JSONObject jsonObject;
@@ -200,14 +190,13 @@ public class C2BController
             jsonObject = c2BTransaction.queryPaymentStatus(stkPushQueryRequest.getCheckoutRequestID());
         } catch (IOException e) {
             LOGGER.error("Query Failed {}", e.getLocalizedMessage());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"Internal Server Error"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "Internal Server Error"));
         }
 
         assert jsonObject != null;
         int responseCode = Integer.parseInt(jsonObject.getString("ResponseCode"));
-        if (responseCode != 0)
-        {
-            return ResponseEntity.status(500).body(new ApiResponse(false,"An Error Occurred Try Again"));
+        if (responseCode != 0) {
+            return ResponseEntity.status(500).body(new ApiResponse(false, "An Error Occurred Try Again"));
         }
 
         return ResponseEntity.status(200).body(jsonObject.toMap());
@@ -224,11 +213,9 @@ public class C2BController
             @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")
     })
     @PostMapping("/initiate")
-    public ResponseEntity<?> initiate(@RequestBody C2BRequest c2BRequest)
-    {
+    public ResponseEntity<?> initiate(@RequestBody C2BRequest c2BRequest) {
         Users user = userRepository.findById(Long.parseLong("1")).orElse(null);
-        if (user == null)
-        {
+        if (user == null) {
             return ResponseEntity.status(404).body(new ResourceNotFoundException("User", "Id", Long.parseLong("1")));
         }
 
@@ -239,12 +226,12 @@ public class C2BController
                     c2BRequest.getAmount(), c2BRequest.getPhoneNumber());
         } catch (IOException e) {
             LOGGER.error("Transaction Failed {}", e.getLocalizedMessage());
-            return ResponseEntity.status(500).body(new ApiResponse(false,"Internal Server Error"));
+            return ResponseEntity.status(500).body(new ApiResponse(false, "Internal Server Error"));
         }
 
         assert jsonObject != null;
-        if (!jsonObject.getString("ResponseCode").equals("0")){
-            return ResponseEntity.status(500).body(new ApiResponse(false,"An Error Occurred Try Again"));
+        if (!jsonObject.getString("ResponseCode").equals("0")) {
+            return ResponseEntity.status(500).body(new ApiResponse(false, "An Error Occurred Try Again"));
         }
 
         C2BModel c2BModel = new C2BModel();
@@ -263,14 +250,13 @@ public class C2BController
     }
 
     @PostMapping(value = "/stkpush/callback")
-    public void confirmation(@RequestBody HashMap<String, Object> jsonStringResponse)
-    {
+    public void confirmation(@RequestBody HashMap<String, Object> jsonStringResponse) {
         JSONObject jsonObject = new JSONObject(jsonStringResponse);
         JSONObject jsonChildObject = (JSONObject) jsonObject.get("Body");
         STKPushCallback result = null;
 
         try {
-           result = new ObjectMapper().readValue(jsonChildObject.toString(),
+            result = new ObjectMapper().readValue(jsonChildObject.toString(),
                     STKPushCallback.class);
         } catch (IOException exception) {
             exception.printStackTrace();
@@ -282,12 +268,11 @@ public class C2BController
         String checkoutRequestID = result.getResult().getCheckoutRequestID();
 
         StkPushModel stkPushModel = stkPushRepository.getByCheckoutRequestID(checkoutRequestID).orElse(null);
-        assert stkPushModel !=  null;
+        assert stkPushModel != null;
 
-        if (!resultCode.equals("0"))
-        {
+        if (!resultCode.equals("0")) {
             stkPushModel.setTransactionComplete((short) 2);
-        }else{
+        } else {
             stkPushModel.setTransactionComplete((short) 1);
         }
 
@@ -296,7 +281,7 @@ public class C2BController
     }
 
     @PostMapping(value = "/validation")
-    public ResponseEntity<?> validationRequest(@RequestBody Map<String, Object> account){
+    public ResponseEntity<?> validationRequest(@RequestBody Map<String, Object> account) {
 
         try {
             LOGGER.info(new ObjectMapper().writeValueAsString(account));
@@ -306,14 +291,14 @@ public class C2BController
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("ResultCode", 0);
-        jsonObject.put("ResultDesc","Accepted");
+        jsonObject.put("ResultDesc", "Accepted");
 
         //Get back to this
         return ResponseEntity.status(200).body(jsonObject.toMap());
     }
 
     @PostMapping(value = "/confirmation")
-    public ResponseEntity<?> validationConfirmationResponse(@RequestBody Map<String, Object> account){
+    public ResponseEntity<?> validationConfirmationResponse(@RequestBody Map<String, Object> account) {
         //isuue
         C2BModel c2BModel = c2BRespository.getByTransID((String) account.get("TransID")).orElse(null);
         assert c2BModel != null;
